941f364ed838dd79a9039d572a9dde48479e2175
Daniela <danny.cahuantzi@gmail.com>
2020-10-07 22:35:49 -0500

    nombres eliminados

--- Prueba1.java
+++ Prueba1.java
@@ -17,7 +17,6 @@ public class Prueba1 {
     public static void main(String[] args) {
         System.out.println("Daniela");
         System.out.println("Rommy");
-        System.out.println("Jefazo");
     }
     
 }
